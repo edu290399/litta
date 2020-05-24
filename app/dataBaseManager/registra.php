e93635222ce5081481877a1689eff86bf17a3766
Eduardo Silvestre <eduardospg2903@gmail.com>
2020-05-24 15:24:00 -0300

    verifica se ja existe um email validado

--- app/dataBaseManager/registra.php
+++ app/dataBaseManager/registra.php
@@ -18,10 +18,33 @@ if($btnCadastra){
 	$senha = password_hash($senha, PASSWORD_DEFAULT);
 	$sql = "INSERT INTO `usuarios`( `nome`, `sobrenome`, `sexo`, `datanas`, `telefone1`, `telefone2`, `email`, `usuario`, `senha`, `pais`, `estado`, `cidade`) VALUES (?, ?, ?, ?, ?, ?, ?, ? ,?, ?,?,?)";
 	$sqlUsuario = "SELECT id FROM usuarios where usuario = ? LIMIT 1";
-	if( ($stmt = mysqli_prepare($conn, $sql)) && ($stmtUsuario = mysqli_prepare($conn, $sqlUsuario)) ){
+	$sqlEmail = "SELECT id FROM usuarios where email = ? AND confirmado = '1' LIMIT 1";
+	if( ($stmt = mysqli_prepare($conn, $sql)) && ($stmtUsuario = mysqli_prepare($conn, $sqlUsuario)) && ($stmtEmail = mysqli_prepare($conn,$sqlEmail)) ){
 		mysqli_stmt_bind_param($stmt, "ssssssssssss", $nome,$sobrenome,$sexo,$datanas,$telefone1,$telefone2,$email,$usuario,$senha,$pais,$estado,$cidade);
+		mysqli_stmt_bind_param($stmtEmail, "s", $email);
 		mysqli_stmt_bind_param($stmtUsuario, "s", $usuario);
+		if( mysqli_stmt_execute($stmtEmail) ){
+			mysqli_stmt_bind_result($stmtEmail, $email);
+		} 
+		else{
+			echo "ERROR: Could not execute query: Consulta no BD de email. \n" .  mysqli_stmt_error($stmtEmail) . "\n"  .mysqli_error($conn);
+		}
+		if(mysqli_stmt_fetch($stmtEmail)){
+			$_SESSION['msgErro'] = "Email já está em uso \n";
+			$_SESSION['nome'] = $nome;
+			$_SESSION['sobrenome'] = $sobrenome;
+			$_SESSION['sexo'] = $sexo;
+			$_SESSION['datanas'] = $datanas;
+			$_SESSION['telefone1'] = $telefone1;
+			$_SESSION['telefone2'] = $telefone2;
+			$_SESSION['usuario'] = $usuario;
+			$_SESSION['pais'] = $pais;
+			$_SESSION['estado'] = $estado;
+			$_SESSION['cidade'] = $cidade;
 
+			header("Location: ../register");
+		}
+		else{
 			if(mysqli_stmt_execute($stmtUsuario) ){
 				mysqli_stmt_bind_result($stmtUsuario, $usuario);
 			}
@@ -46,7 +69,7 @@ if($btnCadastra){
 				if(mysqli_stmt_execute($stmt)){
 					$link = "https://littadesign.com/confirmacaoCadastro?utilizador=$usuario";
 					if( mail($email, 'Cadastro LITTA', 'Olá '.$email.', clique aqui para confirmar o cadastro '.$link) ){
-						$_SESSION['msgOk'] ="Verifique a caixa de entrada de <br> <strong> .$email. </strong> <br>Para confirmar o cadastro <br>Caso não encontre, verifique seus spams.";
+						$_SESSION['msgOk'] ="Verifique a caixa de entrada de <br> <strong> $email </strong> <br>Para confirmar o cadastro <br>Caso não encontre, verifique seus spams.";
 						header("Location: ../login");  
 					  } else {
 						echo '<p>Houve um erro ao enviar o email (o servidor suporta a função mail?)</p>';
@@ -57,6 +80,7 @@ if($btnCadastra){
 					header("Location: ../register");
 				}
 			}
+		}	
 	} 
 	else{
 		echo "ERROR: Could not prepare query to access DB. " . mysqli_error($conn);
